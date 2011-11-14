c9803aba76a891af24b74572ddf5264930848093
exithosix <exithosix@gmail.com>
2011-11-14 14:55:59 +0000

    расчехляем Веб браузер

--- ULogin.pas
+++ ULogin.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, ExtCtrls;
+  Dialogs, StdCtrls, ExtCtrls, UAcaunt;
 
 type
   TfrmLogin = class(TForm)
@@ -19,17 +19,38 @@ type
     cbbAgetn: TComboBox;
     lblServer: TLabel;
     edtServer: TEdit;
+    btnOk: TButton;
+    procedure btnOkClick(Sender: TObject);
   private
     { Private declarations }
+    FAcaunt : TAcaunt;
   public
     { Public declarations }
+    procedure Show(AAcaunt: TAcaunt);
   end;
-
-//var
-//  frmLogin: TfrmLogin;
 
 implementation
 
 {$R *.dfm}
+
+{ TfrmLogin }
+
+procedure TfrmLogin.btnOkClick(Sender: TObject);
+begin
+  FAcaunt.NameServer := edtServer.Text;
+  FAcaunt.Login := edtLogin.Text;
+  FAcaunt.Password := edtPass.Text;
+  FAcaunt.UserAgetn := cbbAgetn.Text;
+  {FAcaunt.IsProxy :=
+  FAcaunt.ProxyName :=
+  FAcaunt.ProxyPort :=  }
+  self.Close;
+end;
+
+procedure TfrmLogin.Show(AAcaunt: TAcaunt);
+begin
+  FAcaunt:= AAcaunt;
+  self.ShowModal;
+end;
 
 end.
