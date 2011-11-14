23cf3ecd5dc18baf263d8bb8d0bfceab093ee4ca
exithosix@gmail.com <exithosix@gmail.com@db6b144a-afd3-3de3-2c48-4033f1be62e6>
2011-11-14 09:35:31 +0000

    чето добавил но вот че ...

---
+++ trunk/UAcaunt.pas
@@ -0,0 +1,17 @@
+unit UAcaunt;
+
+interface
+
+Type
+  Acaunt = class
+  private
+  public
+    Nameserver: String;  //имя серваре например http://aburabur.blabla.ru
+    Login: String;       //логин пользователя
+    Password: String;    //пароль пользователя
+end;
+
+
+implementation
+
+end.
